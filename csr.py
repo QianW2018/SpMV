3c5b514c948389d4e50532e7bcf76ea6bd280b54
QW <43798833+QianW2018@users.noreply.github.com>
2019-04-06 02:27:24 -0400

    CSR
    format and kernel

---
+++ csr.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+#CSR format
+A = np.array([[1,0,0,0,0],[0,0,2,0,3],[0,4,0,0,5],[0,0,6,0,0],[0,0,0,7,0],[0,0,0,0,8]])
+B = np.array([[1],[2],[3],[4],[5]])
+
+
+rowNum = int(A.shape[0])
+columnNum = int(A.shape[1])
+Value = np.array([])
+Column = np.array([])
+RowPtr = np.array([])
+Result = np.array([])
+
+
+for i in range(rowNum):
+    flag = 1;
+    for j in range(columnNum):
+        if A[i][j] != 0:
+           Value = np.append(Value, np.array(A[i][j]))
+           Column = np.append(Column, j)
+           if flag == 1:
+               RowPtr = np.append(RowPtr, len(Value)-1)
+               flag = 0;
+
+RowPtr = np.append(RowPtr, 8)
+
+print (Value)
+print (Column)
+print (RowPtr)
+
+
+#CSR kernel: A x B
+for i in range(rowNum):
+    start = int(RowPtr[i])
+    end = int(RowPtr[i + 1])
+    temp = 0
+    for j in range(start, end):
+        k = int(Column[j])
+        temp += Value[j] * B[k][0]
+    Result = np.append(Result, temp)
+
+
+print (Result)
